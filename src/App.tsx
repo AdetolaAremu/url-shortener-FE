d3fa18d22825b0ea6a0b042a67a303bd6f979d86
AdetolaAremu <aremutola@gmail.com>
2025-04-30 23:00:26 +0100

    initial setup of the frontend

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-blue-50 h-screen flex items-center justify-center">
+      <div className="font-bold text-5xl text-blue-400 text-center">
+        Create Short Link
+      </div>
+    </div>
+  );
+}
+
+export default App;
